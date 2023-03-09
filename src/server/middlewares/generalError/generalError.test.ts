795825a7cbe7058c36aa4f9bc3e2f2ebdfdb75d0
Sergio <sergiot.fontal@gmail.com>
2023-03-09 12:01:43 +0100

    Fix generalError test: type assertions

--- src/server/middlewares/generalError/generalError.test.ts
+++ src/server/middlewares/generalError/generalError.test.ts
@@ -1,13 +1,13 @@
-import type { NextFunction, Request, Response } from "express";
+import type { Request, Response } from "express";
 import CustomError from "../../../CustomError/CustomError.js";
 import { generalError } from "./generalError.js";
 
-const res = {
+const res: Partial<Response> = {
   status: jest.fn().mockReturnThis(),
   json: jest.fn(),
-} as Partial<Response>;
-const req = {} as Request;
-const next = {} as NextFunction;
+};
+const req: Partial<Request> = {};
+const next = jest.fn();
 
 describe("Given a generalError middleware", () => {
   describe("When it receives an error with a 404 statusCode", () => {
@@ -19,7 +19,7 @@ describe("Given a generalError middleware", () => {
         "Somethig went wrong"
       );
 
-      generalError(error, req, res as Response, next);
+      generalError(error, req as Request, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(statusCode);
     });
@@ -30,18 +30,18 @@ describe("Given a generalError middleware", () => {
       const statusCode = 500;
       const error = new Error();
 
-      generalError(error as CustomError, req, res as Response, next);
+      generalError(error as CustomError, req as Request, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(statusCode);
     });
   });
 
   describe("When it receives an error with an empty publicMessage", () => {
-    test("Then it should call its publicMessage method with 'Internal server error'", () => {
+    test("Then it should call its json method with 'Internal server error'", () => {
       const publicMessage = { error: "Internal server error" };
       const error = new Error();
 
-      generalError(error as CustomError, req, res as Response, next);
+      generalError(error as CustomError, req as Request, res as Response, next);
 
       expect(res.json).toHaveBeenCalledWith(publicMessage);
     });
