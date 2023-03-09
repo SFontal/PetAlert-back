0c6eeececbcde3b5c59fed7e4b6cc512416eac98
Sergio <sergiot.fontal@gmail.com>
2023-03-09 15:03:37 +0100

    Create connectDataBase function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import "./loadEnvirontment.js";
+import mongoose from "mongoose";
 import startServer from "./server/routers/startServer.js";
+import connectDatabase from "./database/connectDataBase.js";
 
 const port = process.env.PORT ?? 4000;
+const mongoDbUrl = process.env.MONGODB_URL!;
 
 await startServer(+port);
+await connectDatabase(mongoDbUrl);
