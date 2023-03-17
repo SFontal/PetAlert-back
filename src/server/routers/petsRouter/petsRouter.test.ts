d674dfb0985b6e0b13d5b557ec16d9f9474da568
Sergio <sergiot.fontal@gmail.com>
2023-03-17 16:43:21 +0100

    Test petsRouter

---
+++ src/server/routers/petsRouter/petsRouter.test.ts
@@ -0,0 +1,45 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import connectDatabase from "../../../database/connectDataBase.js";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../index.js";
+import Pet from "../../../database/models/Pet.js";
+
+let mongodbServer: MongoMemoryServer;
+const getPetsPath = "/pets";
+
+beforeAll(async () => {
+  mongodbServer = await MongoMemoryServer.create();
+  const mongoServerUrl = mongodbServer.getUri();
+
+  await connectDatabase(mongoServerUrl);
+  await Pet.create({
+    name: "Mute",
+    born: new Date(),
+    gender: "Male",
+    specie: "Dog",
+    breed: "French Bulldog",
+    colour: "White",
+    size: "Small",
+    description:
+      "Miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau miau.",
+    imageUrl: "Mute.jpg",
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongodbServer.stop();
+});
+
+describe("Given a GET '/pets' endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should call its method status with 200", async () => {
+      const expectedStatusCode = 200;
+
+      const response = await request(app)
+        .get(getPetsPath)
+        .expect(expectedStatusCode);
+    });
+  });
+});
