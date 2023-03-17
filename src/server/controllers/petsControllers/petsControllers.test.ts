5c2478230395f9271f11528a35ea477d50064a2a
Sergio <sergiot.fontal@gmail.com>
2023-03-17 14:17:52 +0100

    Test getPets controller

---
+++ src/server/controllers/petsControllers/petsControllers.test.ts
@@ -0,0 +1,54 @@
+import type { Request, Response } from "express";
+import CustomError from "../../../CustomError/CustomError";
+import Pet from "../../../database/models/Pet";
+import { type PetStructure, type PetsStructure } from "../types";
+import { getPets } from "./petsControllers";
+
+const req: Partial<Request> = {};
+const res: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+const next = jest.fn();
+
+describe("Given a getPets controller", () => {
+  describe("When it receives a response and yo k sé, tío", () => {
+    test("Then it should call its next method with a status 500 and a message 'Couldn't retrieve pets!'", async () => {
+      const pets: PetsStructure = [];
+      Pet.find = jest.fn().mockResolvedValue(pets);
+
+      const statusCode = 500;
+      const message = "Couldn't retrieve pets!";
+      const publicMessage = message;
+      const expectedError = new CustomError(message, statusCode, publicMessage);
+
+      await getPets(req as Request, res as Response, next);
+
+      expect(next).toBeCalledWith(expectedError);
+    });
+  });
+
+  describe("When it receives a response with list of one pet", () => {
+    const pet: Partial<PetStructure> = {};
+
+    test("Then it should call its method status with a 200", async () => {
+      const pets: PetsStructure = [pet as PetStructure];
+      Pet.find = jest.fn().mockResolvedValue(pets);
+
+      const statusCode = 200;
+
+      await getPets(req as Request, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+
+    test("Then it should call its method json with an object with a property pets", async () => {
+      const pets: PetsStructure = [pet as PetStructure];
+      Pet.find = jest.fn().mockResolvedValue(pets);
+
+      await getPets(req as Request, res as Response, next);
+
+      expect(res.json).toHaveBeenCalledWith({ pets });
+    });
+  });
+});
